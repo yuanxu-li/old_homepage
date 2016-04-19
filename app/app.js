f9438cdcd7c33cec47cca0092382fb330f09b6e6
Yuanxu Li <yuanxu.lee@gmail.com>
2016-04-19 01:56:31 -0400

    move nav and footer to separate files. defactoring

--- app/app.js
+++ app/app.js
@@ -4,8 +4,22 @@ app.controller("homeCtrl", function($scope) {
 	$scope.welcome = "Welcome to Yuanxu Li's Homepage";
 });
 
+app.directive("navigation", function() {
+	return {
+		restrict: 'E',
+		templateUrl: '/views/navigation.html'
+	};
+});
+
+app.directive("footer", function() {
+	return {
+		restrict: 'E',
+		templateUrl: '/views/footer.html'
+	};
+});
+
 app.filter('filter', function() {
 	return function(x) {
 		return x;
-	}
+	};
 })
