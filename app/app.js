18568d4c1e5a9e578b4394d2e4a9be5b1217578d
Yuanxu Li <yuanxu.lee@gmail.com>
2016-04-19 13:40:43 -0400

    clear

--- app/app.js
+++ app/app.js
@@ -4,10 +4,21 @@ app.controller("homeCtrl", function($scope) {
 	$scope.welcome = "Welcome to Yuanxu Li's Homepage";
 });
 
+
 app.directive("navMenu", function() {
 	return {
 		restrict: 'E',
-		templateUrl: '/views/_navigation.html'
+		templateUrl: '/views/_navigation.html',
+		controller: function($scope) {
+			$scope.tab = 1;
+			$scope.isSelected = function(checkTab) {
+				return $scope.tab === checkTab;
+			};
+			$scope.selectTab = function(setTab) {
+				$scope.tab = setTab;
+			};
+		},
+		controllerAs: 'nav'
 	};
 });
 
